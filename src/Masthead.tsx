e80bf89ba6f15c746f9c2b3545218cb54336840b
Stephen Vector <stephenvector@pm.me>
2019-09-30 00:49:46 -0400

    Copy change.

--- src/Masthead.tsx
+++ src/Masthead.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Masthead() {
   return (
     <div className="Masthead">
-      <h1>A simple todo list for complex people.</h1>
+      <h1>A simple task list for complex people.</h1>
     </div>
   );
 }
